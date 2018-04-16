7e8ce74bcff671d349c37941c5bf465f11456ea1
ikelasid <ikelasid@gmail.com>
2018-04-16 18:07:19 +0300

    Fix password validation for stronger passwords

--- app/src/main/java/com/example/android/eventory/Utils/SignUpCredentialsChecker.java
+++ app/src/main/java/com/example/android/eventory/Utils/SignUpCredentialsChecker.java
@@ -12,6 +12,36 @@ public class SignUpCredentialsChecker {
     }
 
     public static boolean isPasswordValid(String password) {
-        return password.length()>6;
+        boolean hasLetter=false;
+        boolean hasDigit = false;
+        if (password.length() >= 8) {
+            for (int i = 0; i < password.length(); i++) {
+                char x = password.charAt(i);
+                if (Character.isLetter(x)) {
+
+                    hasLetter = true;
+                }
+
+                else if (Character.isDigit(x)) {
+
+                    hasDigit = true;
+                }
+
+                // no need to check further, break the loop
+                if(hasLetter && hasDigit){
+
+                    break;
+                }
+
+            }
+            if (hasLetter && hasDigit) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            System.out.println("HAVE AT LEAST 8 CHARACTERS");
+        }
+        return false;
     }
 }
